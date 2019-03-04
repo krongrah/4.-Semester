baaa070fb1397f2142863520a0d403caee8a70ad
Ahmad Hamid <Ahham14@student.sdu.dk>
2019-03-04 14:26:35 +0100

    Project init
    Co-Authored-By: Krongrah <krongrah@users.noreply.github.com>

---
+++ Common/src/main/java/game/common/World.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.common;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author ahmadhamid
+ */
+public class World {
+    private final Map<String, Entity> entityMap = new ConcurrentHashMap<>();
+
+    public String addEntity(Entity entity) {
+        entityMap.put(entity.getID(), entity);
+        return entity.getID();
+    }
+
+    public void removeEntity(String entityID) {
+        entityMap.remove(entityID);
+    }
+
+    public void removeEntity(Entity entity) {
+        entityMap.remove(entity.getID());
+    }
+    
+    public Collection<Entity> getEntities() {
+        return entityMap.values();
+    }
+
+    public <E extends Entity> List<Entity> getEntities(Class<E>... entityTypes) {
+        List<Entity> r = new ArrayList<>();
+        for (Entity e : getEntities()) {
+            for (Class<E> entityType : entityTypes) {
+                if (entityType.equals(e.getClass())) {
+                    r.add(e);
+                }
+            }
+        }
+        return r;
+    }
+
+    public Entity getEntity(String ID) {
+        return entityMap.get(ID);
+    }
+}
