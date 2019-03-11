b5f1c8b0d1a5d131bfc51c026b2ae431fe8b595b
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2019-03-11 14:20:39 +0100

    Implemented player and enemy
    Small change in MovingPart

---
+++ Project/Enemy/src/main/java/game/enemy/Enemy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.enemy;
+
+import common.Entity;
+import sprites.Sprites;
+
+/**
+ *
+ * @author andreasmolgaard-andersen
+ */
+public class Enemy extends Entity {
+    
+    public Enemy(Sprites sprite) {
+        super(sprite);
+    }
+    
+}
