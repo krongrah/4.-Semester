5eedf8292a0b6ad9cae7de34dbade697c9a3f6cc
Krongrah <Krongrah@gmail.com>
2019-03-14 14:19:55 +0100

    temp Sprite

--- Project/Renderer/src/main/java/Renderer/Renderer.java
+++ Project/Renderer/src/main/java/Renderer/Renderer.java
@@ -3,18 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package renderer;
+package Renderer;
 
+import assetmanagement.Assets;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import data.GameData;
 import data.World;
 import map.Map;
 import services.IRenderer;
+import sprites.Sprites;
 
 /**
  *
@@ -25,8 +28,12 @@ public class Renderer implements IRenderer{
     private OrthographicCamera camera;
     private TiledMapRenderer tiledMapRenderer;
     private Color backgroundColor;
+    private Assets assetmanager;
+    private SpriteBatch batch;
 
     public Renderer() {
+        assetmanager=new Assets();
+        batch=new SpriteBatch();
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
 
@@ -46,10 +53,23 @@ public class Renderer implements IRenderer{
         tiledMapRenderer.render();
 
         camera.position.set(gameData.getFocusX(), gameData.getFocusY(), 0); //X, Y, Z coordinates
+        draw();
+        
+    
     }
 
     @Override
     public void setBackgroudColor(float r, float g, float b, float a) {
         this.backgroundColor = new Color(r,g,b,a);
     }
+    
+    private void draw(){
+    batch.begin();
+    
+    assetmanager.getSprite(Sprites.LUKE).draw(batch);
+    
+    batch.end();
+    }
+    
+    
 }
