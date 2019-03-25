bddd24c6c111498274ba6300a6d5aa863dbaf086
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2019-03-25 15:04:25 +0100

    skub

--- StarWars/Core/src/main/java/game/core/Game.java
+++ StarWars/Core/src/main/java/game/core/Game.java
@@ -56,6 +56,7 @@ public class Game implements ApplicationListener {
 
         cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
         cam.translate(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        cam.zoom = 2;
         cam.update();
 
         sr = new ShapeRenderer();
