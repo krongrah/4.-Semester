db094750aa3a8a2cd82ea701049bfcad2b8e6293
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2019-03-29 23:54:59 +0100

    Fixed collision both Left & Right

--- StarWars/Collision/src/main/java/game/collision/Collision.java
+++ StarWars/Collision/src/main/java/game/collision/Collision.java
@@ -43,88 +43,41 @@ public class Collision implements IPostProcessor {
                     PropertiesPart tarProp = target.getPart(PropertiesPart.class);
                     if (object != target && tarProp.isSolid()) {
 
-                        float dxL = ((objPos.getX() - tarPos.getX()) + (objProp.getWidth() + tarProp.getWidth()));
-                        float dxR = ((tarPos.getX() - objPos.getX()) + ((tarProp.getWidth() / 2) - (objProp.getWidth() / 2)));
-//                        System.out.println("Coordinates:");
-//                        System.out.println("Object: (" + objPos.getX() + ", " + objPos.getY() + ")");
-//                        System.out.println("Target: (" + tarPos.getX() + ", " + tarPos.getY() + ")");
-                        System.out.println("X-Distance, Right: " + dxR + " Left: " + dxL);
-//                        System.out.println("\nHeight x Width:");
-//                        System.out.println("Object: " + objProp.getHeight() + " x " + objProp.getWidth());
-//                        System.out.println("Target: " + tarProp.getHeight() + " x " + tarProp.getWidth());
-//                        System.out.println("\n\n");
+                        //System.out.println("X-Distance, Right: " + dxR + " Left: " + dxL);
+                        if (objPos.getX() <= tarPos.getX()) {
+                            //Check for right side collision exclusively                        
+                            float dxR = Math.abs((tarPos.getX() - objPos.getX()) + ((tarProp.getWidth() / 2) - (objProp.getWidth() / 2)));
 
-                        if (dxR <= 1 ) {
-                            System.out.println("Right collision by " + dxR);
-                            setXAxisCollision(object, target);
-                        } else {
+                            if (dxR < objProp.getWidth()/2) {
+                                //Collision detected:
+                                object.setCollisionDirection(Directions.RIGHT);
+                                object.setCollision(CollisionTypes.SOLIDOBJECT);
+
+                            }
+                        }
+                        else if (objPos.getX() >= tarPos.getX()) {
+                            float dxL = Math.abs((objPos.getX() - tarPos.getX()) - ((objProp.getWidth() / 2) + tarProp.getWidth() / 2));
+
+                            //Check for left side collision exclusively
+                            if (dxL <= objProp.getWidth()/2) {
+                                //Collision detected:
+                                object.setCollisionDirection(Directions.LEFT);
+                                object.setCollision(CollisionTypes.SOLIDOBJECT);
+                            }
+                        }else {
                             resetEntityCollision(object);
                         }
-                        if ((objPos.getY() + objProp.getHeight()) > (tarPos.getY() + tarProp.getHeight())) {
-                            //setYAxisCollision(object, target);
 
-                        }
+//                        if ((objPos.getY() + objProp.getHeight()) > (tarPos.getY() + tarProp.getHeight())) {
+//                            //setYAxisCollision(object, target);
+//
+//                        }
                     }
                 }
             }
         }
     }
 
-    /**
-     * Sets a specific collision for the object only in regards to the y-axis
-     *
-     * @param object : Any Entity that moves
-     * @param target : Any obstacle
-     */
-    private void setYAxisCollision(Entity object, Entity target) {
-        PropertiesPart tarProp = target.getPart(PropertiesPart.class);
-        PositionPart objPos = object.getPart(PositionPart.class);
-        PositionPart tarPos = target.getPart(PositionPart.class);
-
-        object.setCollision(CollisionTypes.SOLIDOBJECT);
-        if (tarProp.isObstacle()) {
-            //Entity has bumped into obstacle on the y-axis:
-            if (tarPos.getY() < objPos.getY()) {
-                //Roof collision:
-                object.setCollisionDirection(Directions.UP);
-            } else {
-                //Floor collision:
-                object.setCollisionDirection(Directions.DOWN);
-            }
-
-        }
-    }
-
-    /**
-     * Sets a specific collision for the object only in regards to the x-axis
-     *
-     * @param object : Any Entity that moves
-     * @param target : Any obstacle
-     */
-    private void setXAxisCollision(Entity object, Entity target) {
-        PropertiesPart tarProp = target.getPart(PropertiesPart.class);
-        PropertiesPart objProp = object.getPart(PropertiesPart.class);
-        PositionPart objPos = object.getPart(PositionPart.class);
-        PositionPart tarPos = target.getPart(PositionPart.class);
-
-        object.setCollision(CollisionTypes.SOLIDOBJECT);
-
-//        System.out.println("Coordinates:");
-//        System.out.println("Player: (" + objPos.getX() + ", " + objPos.getY() + ")");
-//        System.out.println("Player: (" + tarPos.getX() + ", " + tarPos.getY() + ")\n\n");
-        if (tarProp.isObstacle()) {
-            //Entity has bumped into obstacle on the x-axis:
-            if ((objPos.getX() + objProp.getWidth() / 2) - (tarPos.getX() + tarProp.getWidth() / 2) <= 1) {
-                //Right collision:
-                object.setCollisionDirection(Directions.RIGHT);
-            }
-            if ((tarPos.getX() + tarProp.getWidth() / 2) < (objPos.getX() + objProp.getWidth() / 2)) {
-                //Left collision:
-                //object.setCollisionDirection(Directions.LEFT);
-            }
-
-        }
-    }
 
     private void resetEntityCollision(Entity object) {
         object.setCollision(CollisionTypes.NO_EFFECT);
