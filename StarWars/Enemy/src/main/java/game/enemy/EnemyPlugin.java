c986d4c09fd99b876ff9da61e87b15fe1aa2a46e
Krongrah <Krongrah@gmail.com>
2019-04-04 15:49:41 +0200

    Update EnemyPlugin.java

--- StarWars/Enemy/src/main/java/game/enemy/EnemyPlugin.java
+++ StarWars/Enemy/src/main/java/game/enemy/EnemyPlugin.java
@@ -31,7 +31,7 @@ public class EnemyPlugin implements IPluginService {
     public void start(GameData gameData, World world) {
         enemy = new Enemy();
         MovingPart mov=new MovingPart(10, 175, 250);
-        enemy.add(new PropertiesPart(32, 32, CollisionTypes.SOLIDOBJECT,false));
+        //enemy.add(new PropertiesPart(32, 32, CollisionTypes.SOLIDOBJECT,false));
         enemy.add(new LifePart(3));
         enemy.add(new PositionPart(25*32, (39*32)+16));
         enemy.add(mov);
@@ -45,7 +45,7 @@ public class EnemyPlugin implements IPluginService {
         
         mov=new MovingPart(10, 100, 100);
         
-        enemy.add(new PropertiesPart(32, 32, CollisionTypes.SOLIDOBJECT,false));
+        //enemy.add(new PropertiesPart(32, 32, CollisionTypes.SOLIDOBJECT,false));
         enemy.add(new LifePart(3));
         enemy.add(new PositionPart(10*32, (39*32)+16));
         enemy.add(mov);
