a0e8c43fb407100b380ad24e2713c717bf447016
Sebastian Christiansen <32192177+Sebastianchr22@users.noreply.github.com>
2019-04-04 15:54:52 +0200

    Environments, Behaviours

--- StarWars/Enemy/src/main/java/game/enemy/EnemyProcessor.java
+++ StarWars/Enemy/src/main/java/game/enemy/EnemyProcessor.java
@@ -10,8 +10,11 @@ import data.GameData;
 import data.World;
 import entityparts.MovingPart;
 import entityparts.PositionPart;
+import entityparts.PropertiesPart;
+import enums.Behaviours;
+import enums.Environments;
+import interfaces.Targetable;
 import org.openide.util.lookup.ServiceProvider;
-import services.IPluginService;
 import services.IProcessor;
 
 @ServiceProvider(service = IProcessor.class)
@@ -22,20 +25,91 @@ import services.IProcessor;
  */
 public class EnemyProcessor implements IProcessor {
 
+    private int range = 5 * 32; //was 12
+    private Environments targetDirection;
+    private Behaviours action;
+    private boolean lineOfSight;
+
     @Override
     public void process(GameData gameData, World world) {
-        
-        
+
         for (Entity enemy : world.getEntities(Enemy.class)) {
-            //PositionPart pos=enemy.getPart(PositionPart.class);
-            
             MovingPart mp = enemy.getPart(MovingPart.class);
 
+            //Analyze environment
+            switch (analyze(world, enemy)) {
+                case LEFT:
+                    //React
+                    mp.setLeft(true);
+                    mp.setRight(false);
+                    break;
+                case RIGHT:
+                    //React
+                    mp.setRight(true);
+                    mp.setLeft(false);
+                    break;
+                case LINEOFSIGHT:
+                    //React
+                    break;
+                case OUTOFSIGHT:
+                    //React
+                    break;
+            }
+
+            //PositionPart pos=enemy.getPart(PositionPart.class);
             mp.process(gameData, enemy);
-            
         }
-        
-        
+
     }
-    
+
+    private Environments analyze(World world, Entity enemy) {
+        //Draw straight line between enemy and player
+        //Meassure distance to ensure player is within radius
+
+        PositionPart enPos = enemy.getPart(PositionPart.class);
+        PropertiesPart enProp = enemy.getPart(PropertiesPart.class);
+
+        for (Entity ent : world.getEntities()) {
+            if (ent instanceof Targetable) {
+                //Found player
+                PositionPart plPos = ent.getPart(PositionPart.class);
+
+                float dx = plPos.getX() - enPos.getX();
+                float dy = plPos.getY() - enPos.getY();
+                float yGive = enProp.getHeight() / 2;
+
+                if (Math.abs(dx) > range) {
+                    lineOfSight = false;
+                } else {
+                    if (dy < yGive) {
+                        if (dx < 0) {
+                            //Player is left
+                            targetDirection = Environments.LEFT;
+                        }
+                        if (dx > 0) {
+                            //Player is right
+                            targetDirection = Environments.RIGHT;
+                        }
+                    } else {
+                        lineOfSight = false;
+                    }
+                }
+
+                //Decide on an action:
+                if (Math.abs(dx) > range * 0.75) {
+                    lineOfSight = true;
+                    action = Behaviours.WALK;
+                } else if (Math.abs(dx) <= range * 0.75) {
+                    lineOfSight = true;
+                    action = Behaviours.SHOOT;
+                } else {
+                    action = Behaviours.IDLE;
+                }
+
+            }
+        }
+
+        return null;
+    }
+
 }
