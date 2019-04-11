65b8ed85e908b99ec3bff8f886d2f05a472bcd2f
Kijav16 <kijav16@student.sdu.dk>
2019-04-11 13:06:44 +0200

    sounds component

---
+++ StarWars/sound/src/main/java/game/sound/soundCommands.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.sound;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Naik
+ */
+public class soundCommands {
+    private static HashMap<String, Sound> sounds;
+	
+	static {
+		sounds = new HashMap<String, Sound>();
+	}
+	
+	public static void load(String path, String name) {
+		Sound sound = Gdx.audio.newSound(Gdx.files.internal(path));
+		sounds.put(name, sound);
+	}
+	
+	public static void play(String name) {
+		sounds.get(name).play();
+	}
+	
+	public static void loop(String name) {
+		sounds.get(name).loop();
+	}
+	
+	public static void stop(String name) {
+		sounds.get(name).stop();
+	}
+	
+	public static void stopAll() {
+		for(Sound s : sounds.values()) {
+			s.stop();
+		}
+	}
+    
+}
