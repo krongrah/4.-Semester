9b43cbb82c6fb983b99cfa8eec6957b9da94553f
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2019-04-25 12:09:15 +0200

    handle input removed

--- StarWars/Core/src/main/java/gamestates/PlayState.java
+++ StarWars/Core/src/main/java/gamestates/PlayState.java
@@ -121,7 +121,6 @@ public class PlayState extends GameState {
 
     @Override
     public void handleInput() {
-        System.out.println("Handle input");
         gameData.getKeys().update();
     }
 
